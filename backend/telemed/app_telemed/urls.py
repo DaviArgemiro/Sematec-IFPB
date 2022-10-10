d427f119584e6b5f8bbd576482d38874377deed6
Notebook <iagoggurgel25@gmail.com>
2022-10-10 09:25:59 -0300

    adicionando todas as rotas, templates, imagens, estilos

--- backend/telemed/app_telemed/urls.py
+++ backend/telemed/app_telemed/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login.html', views.login, name='login')
 ]
--- backend/telemed/app_telemed/views.py
+++ backend/telemed/app_telemed/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'index.html')
+
+def login(request):
+	return render(request, 'login.html')

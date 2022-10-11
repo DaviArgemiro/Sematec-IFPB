988870af01b6e9cfdfa49d317f40f5b31f49895b
Notebook <iagoggurgel25@gmail.com>
2022-10-11 10:13:52 -0300

    adicionando os modelos

--- backend/telemed/app_telemed/models.py
+++ backend/telemed/app_telemed/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class medico(User):
+	num_pais = models.CharField(max_length = 3)
+	num_ddd = models.CharField(max_length = 3)
+	num_telefone = models.CharField(max_length = 15)
+	crm = models.CharField(max_length = 15)
+	crm_UF = models.CharField(max_length = 2)
+	cep = models.CharField(max_length = 10)
+	endereco = models.CharField(max_length = 50)
+	areaAtuacao = models.CharField(max_length = 50)
+
+class cliente(User):
+	num_pais = models.CharField(max_length = 3)
+	num_ddd = models.CharField(max_length = 3)
+	num_telefone = models.CharField(max_length = 15)
+	cep = models.CharField(max_length = 10)
+	endereco = models.CharField(max_length = 50)
